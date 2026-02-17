c682131cbb0375b69a0ca250526e0ffb21833e79
Arnau Davis <arnau.davis@outlook.com>
2026-02-17 02:00:37 -0500

    fixing vercel issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext } from 'react'
 import { LanguageProvider } from './LanguageProvider.jsx'
-import Header from './components/Header.jsx'
+import Header from './components/header.jsx'
 import Landing from './components/Landing.jsx'
 import Footer from './components/Footer.jsx'
 function App() {
@@ -10,9 +10,7 @@ function App() {
     <>
     <LanguageProvider>
       <Header/>
-      <div className="">
       <Landing/>
-      </div>
       <Footer/>
     </LanguageProvider>
     </>
